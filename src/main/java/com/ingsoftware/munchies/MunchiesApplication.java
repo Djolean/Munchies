070b6a837a20fac5cc0af217174281d485d8b38d
djolean <djoler001@gmail.com>
2024-02-09 17:15:10 +0100

    Slack integration setup part 2

--- src/main/java/com/ingsoftware/munchies/MunchiesApplication.java
+++ src/main/java/com/ingsoftware/munchies/MunchiesApplication.java
@@ -4,10 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableScheduling
 @ServletComponentScan
 @OpenAPIDefinition(info = @Info(title = "Restaurant APIS", version = "1.0", description = "Restaurant management Apis"))
